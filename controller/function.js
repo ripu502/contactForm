3c3321bbc7721359d8369840ac114f5c0ec0e6ec
ripu502 <ripu502@gmail.com>
2019-05-11 23:51:23 +0530

    simple is done

--- controller/function.js
+++ controller/function.js
@@ -1,17 +1,18 @@
 const keys = require('../config/keys');
 const request = require('request');
+var nodemailer = require('nodemailer');
 
 module.exports.getForm = (req, res, next) => {
-    res.render('form', {siteKey : keys.siteKey});
+    res.render('form', { siteKey: keys.siteKey });
 };
 
 module.exports.postForm = (req, res, next) => {
-   console.log(req.body);
+    console.log(req.body);
     // if the capcha is not selected
     if (req.body.captcha === undefined ||
         req.body.captcha === '' ||
         req.body.captcha === null) {
-        return res.json({ "msg": "Please select captcha first" });
+        return res.json({ "msg": "Fail from me" });
     }
 
     // capcha is selected
@@ -26,9 +27,31 @@ module.exports.postForm = (req, res, next) => {
         console.log(body);
 
         if (body.success !== undefined && !body.success) {
-            return res.json({ "msg": "Failed captcha verification" });
+            return res.json({ "msg": "Fail" });
         }
-        res.json({ "msg": "Sucess" });
+
+
+        // implementing in the most simple way
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: keys.gmailId,
+                pass: keys.gmailPassword
+            }
+        });
+        const mailOptions = {
+            from: req.body.email, // sender address
+            to: req.body.email, // list of receivers
+            subject: req.body.subject, // Subject line
+            html: `<p>${req.body.message}</p>`// plain text body
+          };
+          transporter.sendMail(mailOptions, function (err, info) {
+            if(err)
+              console.log(err)
+            else
+              console.log(info);
+         });
+        res.json({ "msg": "Success" });
         console.log('success');
     });
 };
